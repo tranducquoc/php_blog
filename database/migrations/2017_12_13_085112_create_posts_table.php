02d5e27cd84ce22c1bae7a55fe8eeaa1f1799127
tranducquoc <ducquocb2@gmail.com>
2017-12-13 16:06:06 +0700

    init database

---
+++ database/migrations/2017_12_13_085112_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('url', 255);
+            $table->string('title', 140);
+            $table->string('description', 170);
+            $table->text('content');
+            $table->tinyInteger('blog');
+            $table->integer('category_id');
+            $table->string('image', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
